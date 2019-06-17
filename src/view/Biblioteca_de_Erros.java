1acc8b7c18ce71d9309dac5d1f0cf0ba864c9c8d
Wiliam <Wiliam@DESKTOP-5H8K57I>
2019-06-16 23:29:33 -0300

    85%

--- src/view/Biblioteca_de_Erros.java
+++ src/view/Biblioteca_de_Erros.java
@@ -1,91 +1,104 @@
 package view;
 
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import model.Erro;
+
 
 public class Biblioteca_de_Erros extends Application implements EventHandler<ActionEvent> {
 
-	private TextArea taErros = new TextArea();
-	private Button btnvoltar = new Button("Voltar");
-	private Label lblTitulo1 = new Label("Biblioteca de Erros");
+	private Button btnVoltar = new Button("Voltar");
+	private Label lblTitulo1 = new Label("Biblioteca de erros");
+	private TableView<Erro> tblErro = new TableView<Erro>();
 	Stage p;
 
-	BackgroundImage myBI = new BackgroundImage(new Image("/img/fireconverter.jpg", 490, 330, false, true),
+	private BackgroundImage myBI = new BackgroundImage(new Image("/img/fireconverter.jpg", 490, 330, false, true),
 			BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
 
 	@Override
-	public void start(Stage stage) throws Exception {
+	public void start(Stage palco) throws Exception {
 
-		Pane tela = new Pane();
-		Scene scn = new Scene(tela, 480, 320);
+		VBox principal = new VBox();
+		Scene scene = new Scene(principal, 480, 320);
 
-		taErros.setEditable(false);
-		taErros.setWrapText(true);
-		taErros.setTranslateX(80);
-		taErros.setTranslateY(60);
-		taErros.setPrefWidth(335);
-		taErros.setPrefHeight(175);
+		palco.setTitle("Tela - Conversão de Vídeo");
+		principal.getChildren().add(lblTitulo1);
+		principal.getChildren().add(btnVoltar);
+		principal.getChildren().add(tblErro);
+		principal.setBackground(new Background(myBI));
 
-		lblTitulo1.setTranslateY(20);
+		palco.setMinHeight(320);
+		palco.setMinWidth(480);
+		palco.setResizable(false);
+
+		lblTitulo1.setTranslateY(10);
 		lblTitulo1.setTranslateX(150);
 
-		btnvoltar.setTranslateX(365);
-		btnvoltar.setTranslateY(265);
+		tblErro.setMaxSize(400, 200);
+		tblErro.setTranslateY(5);
+		tblErro.setTranslateX(45);
+		
+		
+		btnVoltar.setTranslateY(250);
+		btnVoltar.setTranslateX(200);
 
-		tela.getChildren().add(taErros);
-		tela.getChildren().add(lblTitulo1);
+		palco.setScene(scene);
 
-		tela.getChildren().add(btnvoltar);
-		tela.setBackground(new Background(myBI));
-
-		stage.setTitle("Tela - Biblioteca de Erros");
-		stage.setScene(scn);
-		stage.setMinHeight(320);
-		stage.setMinWidth(480);
-		stage.setResizable(false);
+		
+		btnVoltar.setOnAction(this);
 
 		lblTitulo1.setFont(Font.font("Righteous", FontWeight.BOLD, FontPosture.ITALIC, 20));
 		lblTitulo1.setTextFill(Color.web("#6b6b6b"));
 
-		p = stage;
+		p = palco;
 
-		stage.show();
+		palco.show();
+
 	}
 
-	public static void main(String[] args) {
+	public static void main(String args[]) {
+
 		Application.launch(args);
+
 	}
+
+	// help - https://docs.oracle.com/javafx/2/ui_controls/combo-box.htm
 
 	@Override
 	public void handle(ActionEvent event) {
 
-		if (event.getTarget() == btnvoltar) {
+		if (event.getTarget() == btnVoltar) {
 
 			Menu_Principal menu = new Menu_Principal();
 			try {
+				p.setHeight(358.5);
+				p.setMinHeight(330);
+				p.setMinWidth(480);
 				menu.start(p);
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
+				
 				e.printStackTrace();
 			}
-		}
+
+		} 
 
 	}
 
